efe1973af1a6542a78707890ea8988e43ab33b79
Matheus Catheringer <matheuscatheringer@gmail.com>
2024-03-12 20:49:35 -0300

    Adicionar arquivos de projeto.

---
+++ CalculaDesconto/Desconto.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CalculaDesconto
+{
+    public class Desconto
+    {
+        public static float PorcentagemValor(float valor_atual, float desconto)
+        {
+            return (valor_atual / 100) * (desconto);
+        }
+        /*public static float ValorFinal(float valor_atual, float desconto) 
+        {
+            float des = PorcentagemValor(float valor_atual, float desconto);
+            return (valor_atual - des);
+        }
+    */}
+}
---
+++ CalculaDesconto/Program.cs
@@ -0,0 +1,14 @@
+﻿// See https://aka.ms/new-console-template for more information
+using CalculaDesconto;
+
+Console.WriteLine("Calculadora de Desconto");
+Console.WriteLine("Informe o valor");
+float valor_atual = float.Parse(Console.ReadLine());
+Console.WriteLine("Informe a porcentagem de desconto");
+float desconto = float.Parse(Console.ReadLine());
+Console.WriteLine($"o valor de desconto é {Desconto.PorcentagemValor(valor_atual, desconto)}");
+
+
+
+
+
